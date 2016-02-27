1c999654b639b723018dfa308c6bc57801aa563d
Danny Sauer <danny@dannysauer.com>
2016-02-27 08:59:40 -0600

    Add DST conversion code Set Time library to use RTC Clean up some unused code Clean up some used code

--- Lee_Retirement/Lee_Retirement.ino
+++ Lee_Retirement/Lee_Retirement.ino
@@ -8,6 +8,8 @@
  * http://www.amazon.com/gp/product/B00LZCTMJM
  */
 
+//#include <Chronos.h>
+
 /* 
  set up for the RTC
  Libraries used are at
@@ -50,8 +52,6 @@ int adc_key_in  = 0;
 
 String line1 = "";
 String line2 = "";
-String secs;
-String key;
 
 #define BrightMax 255
 #define BrightMin 0
@@ -111,6 +111,7 @@ MenuItem mi_retirement_day(   "Retirement Day"   );
 void mcb_backlight(MenuItem* pmi);
 void mcb_backlight(MenuItem* pmi){
   //
+  //EEPROM.update(ADDR_BRIGHTNESS, brightness);
 }
 
 void mcb_retirement_year(MenuItem* pmi);
@@ -129,15 +130,33 @@ void mcb_retirement_day(MenuItem* pmi){
 }
 
 void setup() {
+  // get saved data from EEPROM
+  EEPROM.get(ADDR_RETIRE_TIME, retire_time);
+  EEPROM.get(ADDR_BRIGHTNESS,  brightness);
+
   // define PWM brightness pin for output
   pinMode(PinBacklight, OUTPUT);
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   lcd.clear();
+  analogWrite(PinBacklight, brightness);
 
-  // get saved data from EEPROM
-  EEPROM.get(ADDR_RETIRE_TIME, retire_time);
-  EEPROM.get(ADDR_BRIGHTNESS,  brightness);
+  // configure clock to use RTC
+  if(! RTC.read(tm)){
+    // RTC isn't working
+    while(true){
+      update_lcd("RTC read error :/", String(millis()/1000));
+      delay(250);
+    }
+  }
+  setSyncProvider(RTC.get);
+  if(timeStatus() != timeSet){
+    // RTC isn't set
+    while(true){
+      update_lcd("Time is not set :/", String(millis()/1000));
+      delay(250);
+    }
+  }
 
   // assemble menu
   mm.add_item(&mi_backlight, &mcb_backlight);
@@ -147,69 +166,105 @@ void setup() {
   mm_retirement.add_item(&mi_retirement_day,   &mcb_retirement_day);
 }
 
+// http://www.nist.gov/pml/div688/dst.cfm
+bool in_dst(){
+  // do no-math checks first for speed
+  if( tm.Month > 3 && tm.Month < 11 ){
+    return true;
+  }
+  if( tm.Month < 3 || tm.Month > 11 ){
+    return false;
+  }
+  signed int previousSunday = tm.Day - tm.Wday;
+  // DST starts at 2AM on the second Sunday of March
+  if( tm.Month == 3 ){
+    // Second Sunday would be between the 8th and 15th
+    return previousSunday >= 8;
+  }
+  // DST stops at 2AM on the first Sunday of November
+  if( tm.Month == 11 ){
+    // first Sunday will be between the between the first and the 7th.
+    return previousSunday <= 0;
+  }
+}
+
+void more_bright(){
+  if( brightness >= BrightMax ){
+    brightness = BrightMax;
+  }
+  analogWrite(PinBacklight, brightness);
+}
+
+void less_bright(){
+  if( brightness >= BrightMax ){
+    brightness = BrightMax;
+  }
+  analogWrite(PinBacklight, brightness);
+}
+
+// only update LCD if intended display != what's already displayed
+String prevline1 = "";
+String prevline2 = "";
+void update_lcd(String l1, String l2){
+  if( l1 != prevline1 ){
+    lcd.setCursor(0,0);
+    lcd.print(l1);
+    prevline1 = l1;
+  }
+  if( l2 != prevline2 ){
+    lcd.setCursor(0,1);
+    lcd.print(l2);
+    prevline2 = l2;
+  }
+}
+
+void menuLoop(){
+  return;  
+}
+
 void loop() {
+  // if the RTC isn't working, the rest of this doesn't matter
+  if(RTC.read(tm)){
+  }
+  else{
+    update_lcd("RTC read error :/", String(millis()/1000));
+  }
+
+  // check on the keys
   lcd_key = read_LCD_buttons();
   switch (lcd_key){
     case btnNONE: {
       break;
     }
     case btnRIGHT: {
-      key = "RIGHT";
-      brightness += 1;
-      if( brightness >= BrightMax ){
-        brightness = BrightMax;
-      }
-      EEPROM.update(ADDR_BRIGHTNESS, brightness);
       break;
     }
     case btnLEFT: {
-      key = "LEFT";
-      brightness -= 1;
-      if( brightness <= BrightMin ){
-        brightness = BrightMin;
-      }
-      EEPROM.update(ADDR_BRIGHTNESS, brightness);
       break;
     }
     case btnUP: {
-      key = "UP";
-      lcd.display();
-      brightness = BrightMax;
       break;
     }
     case btnDOWN: {
-      key = "DOWN";
-      brightness = BrightMin;
-      lcd.noDisplay();
       break;
     }
     case btnSELECT: {
-      key = "SELECT";
+      menuLoop();
       break;
     }
   }
-  analogWrite(PinBacklight, brightness);
 
   // assemble first LCD line
-  line1 = "";
   line1 = "ADC:" + String(adc_key_in);
-  while( line1.length() < 16 - key.length() ){
+  while( line1.length() < 16 ){
     line1 = line1 + " ";
   }
-  line1 = line1 + key;
-  lcd.setCursor(0,0);
-  lcd.print(line1);
 
   // assemble second LCD line
   line2 = "bright:" + String(brightness);
-  secs = String(millis()/1000);
-  if(RTC.read(tm)){
-    secs = "ok!" + String(tm.Second);
-  }
-  while( line2.length() < 16 - secs.length() ){
+  while( line2.length() < 16){
     line2 = line2 + " ";
   }
-  line2 = line2 + secs;
-  lcd.setCursor(0,1);
-  lcd.print(line2);
+
+  update_lcd(line1, line2);
 }
